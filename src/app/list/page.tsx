1be850ca85c7eb547673ea34143169191b609d51
riznanasar003 <richurizna10@gmail.com>
2025-07-11 10:28:26 +0530

    vercel deployement update

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,15 @@
 import Filter from '@/components/Filter/Filter';
+import ProductList from '@/components/ProductList';
 import { wixClientServer } from '@/lib/wixClientServer';
 import { Box, Button, Typography } from '@mui/material';
 import Image from 'next/image';
 import React, { Suspense } from 'react';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const ProductListWrapper = dynamic(() => import('./ProductListWrapper'), {
-  ssr: false, // optional: prevents hydration mismatch errors for `useSearchParams`
-});
+// const ProductListWrapper = dynamic(() => import('@/components/ProductList'), {
+//   ssr: false,
+//   loading: () => <Typography sx={{ px: 4, py: 8 }}>Loading products...</Typography>,
+// });
 
 const ListPage = async () => {
   const catSlug = "all-products";
@@ -64,7 +66,7 @@ const ListPage = async () => {
       </Typography>
 
       <Suspense fallback={<Typography sx={{ px: 4, py: 8 }}>Loading products...</Typography>}>
-        <ProductListWrapper
+        <ProductList
           limit={8}
           categoryId={cat.collection?._id || "00000000-0000-0000-0000-000000000001"}
         />
